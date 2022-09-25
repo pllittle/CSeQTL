b5d8cce89e24c3fd55067159eb728944daa2fbb2
pllittle <shadowsaver911@gmail.com>
2022-09-25 02:11:46 -0400

    build

--- debug/build.R
+++ debug/build.R
@@ -1,8 +1,13 @@
 # Create package
 rm(list=ls())
 library(smarter)
-user_dir 	= gsub("\\\\","/",Sys.getenv("USERPROFILE"))
-git_dir 	= file.path(user_dir,"Desktop/github")
+user_dir = gsub("\\\\","/",Sys.getenv("USERPROFILE"))
+if( user_dir == "" ){
+	git_dir = "~/github"
+} else {
+	git_dir = file.path(user_dir,"Desktop/github")
+}
+
 pack			= "CSeQTL"
 pack_dir 	= file.path(git_dir,pack)
 all_packs	= as.character(installed.packages()[,1])
@@ -11,11 +16,15 @@ if( pack %in% all_packs ){
 	q("no")
 }
 
-smart_prepPack(pack_dir = pack_dir,
-	pandoc = "C:/Program Files/RStudio/bin/pandoc",
-	make_vign = FALSE,
-	cran = FALSE,
-	build_dir = NULL)
+if( user_dir == "" ){
+	devtools::install_github("pllittle/CSeQTL")
+} else {
+	smart_prepPack(pack_dir = pack_dir,
+		pandoc = "C:/Program Files/RStudio/bin/pandoc",
+		make_vign = FALSE,
+		cran = FALSE,
+		build_dir = NULL)
+}
 
 ##
 
